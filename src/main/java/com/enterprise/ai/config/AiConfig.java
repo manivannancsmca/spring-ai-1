2385cecd9ddf6d7df71db2fa94333cc466c9fc56
manivannancsmca <manivannancsmca@gmail.com>
2026-08-29 16:48:54 +0530

    AiConfig class added

---
+++ src/main/java/com/enterprise/ai/config/AiConfig.java
@@ -0,0 +1,35 @@
+package com.enterprise.ai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiConfig {
+
+//    @Bean
+//    public ChatMemory chatMemory() {
+//        return MessageWindowChatMemory.builder()
+//                .maxMessages(10) // 10 last messages at once, 11th message -> 1st removed
+//                .build();
+//    }
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(repository)
+                .maxMessages(10)
+                .build();
+    }
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory) {
+        return builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
+    }
+}
