b40cc7e2abd545ea5fc69445ff3669a5ec75c3c3
manivannancsmca <manivannancsmca@gmail.com>
2026-09-04 23:26:58 +0530

    Format ActorsFilms record with an empty line
    Added an empty line for formatting consistency.

--- src/main/java/com/enterprise/ai/dtos/ActorsFilms.java
+++ src/main/java/com/enterprise/ai/dtos/ActorsFilms.java
@@ -2,4 +2,6 @@ package com.enterprise.ai.dtos;
 
 import java.util.List;
 
-public record ActorsFilms(String actor, List<String> movies) {}
+public record ActorsFilms(String actor, List<String> movies) {
+  
+}
